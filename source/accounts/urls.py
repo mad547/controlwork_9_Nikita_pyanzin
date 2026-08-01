200de77dcbb0db563540a93acbbb891f473705d4
mad547 <97340914+mad547@users.noreply.github.com>
2026-08-01 16:07:00 +0600

    добавил urls accounts

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,12 @@
+from accounts.views import RegisterView, ProfileView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('<int:pk>/', ProfileView.as_view(), name='profile'),
 ]
