ba84a0ec9232125d352ea43900716c0e6eeed9c3
mad547 <97340914+mad547@users.noreply.github.com>
2026-08-01 18:27:22 +0600

    настроил права доступа detailстраницы требует авторизации

--- source/photos/views.py
+++ source/photos/views.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import response
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -7,6 +6,14 @@ from photos.models import Photo, Album
 
 
 # Create your views here.
+class AuthorOrPermissionMixin(UserPassesTestMixin):
+    permission_required = None
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user or self.request.user.has_perm(self.permission_required)
+
+
 class PhotoListView(ListView):
     model = Photo
     template_name = 'photos/index.html'
@@ -17,7 +24,7 @@ class PhotoListView(ListView):
         return Photo.objects.filter(is_private=False).select_related('author', 'album')
 
 
-class PhotoDetailView(DetailView):
+class PhotoDetailView(LoginRequiredMixin, DetailView):
     model = Photo
     template_name = 'photos/photo_detail.html'
     context_object_name = 'photo'
@@ -30,27 +37,25 @@ class PhotoCreateView(LoginRequiredMixin, CreateView):
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields['album'].qeryset = Album.objects.filter(author=self.request.user)
+        form.fields['album'].queryset = Album.objects.filter(author=self.request.user)
         return form
 
     def form_valid(self, form):
-        form.instanse.author = self.request.user
+        form.instance.author = self.request.user
         if form.instance.album and form.instance.album.is_private:
             form.instance.is_private = True
-            return super().form_valid(form)
+        return super().form_valid(form)
 
 
-class PhotoUpdateView(LoginRequiredMixin, UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, AuthorOrPermissionMixin, UpdateView):
     model = Photo
     fields = ['image', 'caption', 'album', 'is_private']
     template_name = 'photos/photo_create.html'
-
-    def get_queryset(self):
-        return Photo.objects.filter(album=self.request.user)
+    permission_required = 'photos.change_photo'
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields['album'].qyeryset = Album.objects.filter(author=self.request.user)
+        form.fields['album'].queryset = Album.objects.filter(author=self.request.user)
         return form
 
     def form_valid(self, form):
@@ -59,16 +64,14 @@ class PhotoUpdateView(LoginRequiredMixin, UpdateView):
         return super().form_valid(form)
 
 
-class PhotoDeleteView(LoginRequiredMixin, DeleteView):
+class PhotoDeleteView(LoginRequiredMixin, AuthorOrPermissionMixin, DeleteView):
     model = Photo
     template_name = 'photos/photo_confirm_delete.html'
     success_url = reverse_lazy('photos:index')
-
-    def get_queryset(self):
-        return Photo.objects.filter(author=self.request.user)
+    permission_required = 'photos.delete_photo'
 
 
-class AlbumDetailView(DetailView):
+class AlbumDetailView(LoginRequiredMixin, DetailView):
     model = Album
     template_name = 'photos/album_detail.html'
     context_object_name = 'album'
@@ -84,13 +87,11 @@ class AlbumCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class AlbumUpdateView(LoginRequiredMixin, UpdateView):
+class AlbumUpdateView(LoginRequiredMixin, AuthorOrPermissionMixin, UpdateView):
     model = Album
     fields = ['title', 'description', 'is_private']
     template_name = 'photos/album_create.html'
-
-    def get_queryset(self):
-        return Album.objects.filter(author=self.request.user)
+    permission_required = 'photos.change_album'
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -99,10 +100,8 @@ class AlbumUpdateView(LoginRequiredMixin, UpdateView):
         return response
 
 
-class AlbumDeleteView(LoginRequiredMixin, DeleteView):
+class AlbumDeleteView(LoginRequiredMixin, AuthorOrPermissionMixin, DeleteView):
     model = Album
     template_name = 'photos/album_confirm_delete.html'
     success_url = reverse_lazy('photos:index')
-
-    def get_queryset(self):
-        return Album.objects.filter(author=self.request.user)
+    permission_required = 'photos.delete_album'
